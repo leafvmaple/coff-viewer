c09a8b90d1558ef3e294421eca2cd9c0adbf9567
Zohar Lee <root@leafvmaple.com>
2023-09-25 14:17:03 +0800

    feat: add new csv

--- convert.py
+++ convert.py
@@ -8,7 +8,7 @@ res = []
 with open(path, 'r') as f:
     reader = csv.DictReader(f)
     for line in reader:
-        if line[key] == '' or line[value] == 'Reserved for future use.':
+        if line[value] == '' or line[value] == 'Reserved for future use.':
             continue
 
-        print('"%s": "%s",' % (line[value], line[key]))
+        print('"%s": "%s",' % (line[key], line[value]))
